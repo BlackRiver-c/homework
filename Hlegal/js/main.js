c4388e83199c4abecd51edb4697706258faf4ccf
Serhii Stupka <stupkasergei1@gmail.com>
2024-09-30 23:40:26 +0300

    Hlegal update

--- Hlegal/js/main.js
+++ Hlegal/js/main.js
@@ -1,18 +1,26 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  
+const achievementsSwiper = new Swiper('.achievements-swiper', {
   loop: true,
-
-  // If we need pagination
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+   850: {
+      slidesPerView: 4, 
+    }
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
-
- 
-
-
 });
+
+const newsSwiper = new Swiper('.news-swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+});
+
 document.addEventListener('DOMContentLoaded', function () {
   const burgerButton = document.querySelector('.header__burger');
   const navWrapper = document.querySelector('.header__nav-wr');
